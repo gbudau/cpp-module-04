a46b59754db5adf2e99c5d5f95161e12fbea8eaa
gbudau <54190400+gbudau@users.noreply.github.com>
2021-03-25 14:41:03 +0100

    ex01 Add more tests
    Added more tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,34 +4,131 @@
 #include "SuperMutant.hpp"
 #include "RadScorpion.hpp"
 #include "Character.hpp"
+#include "CyberAnt.hpp"
+#include "AlienAtomizer.hpp"
 
 int	main() {
-	Character* me = new Character("me");
+	{
+		Character* me = new Character("me");
 
-	std::cout << *me;
+		std::cout << *me;
 
-	// This pointer get's deleted in attack()
-	Enemy* b = new RadScorpion();
+		Enemy* b = new RadScorpion();
 
-	AWeapon* pr = new PlasmaRifle();
-	AWeapon* pf = new PowerFist();
+		AWeapon* pr = new PlasmaRifle();
+		AWeapon* pf = new PowerFist();
 
-	me->equip(pr);
-	std::cout << *me;
-	me->equip(pf);
+		me->equip(pr);
+		std::cout << *me;
+		me->equip(pf);
 
-	me->attack(b);
-	std::cout << *me;
-	me->equip(pr);
-	std::cout << *me;
-	me->attack(b);
-	std::cout << *me;
-	me->attack(b);
-	std::cout << *me;
+		me->attack(b);
+		std::cout << *me;
+		me->equip(pr);
+		std::cout << *me;
+		me->attack(b);
+		std::cout << *me;
+		me->attack(b);
+		std::cout << *me;
 
-	delete me;
-	delete pr;
-	delete pf;
+		delete pf;
+		delete pr;
+		delete b;
+		delete me;
+	}
+	std::cout << "\n####### Extra tests #######\n";
+	{
+		Character*	khans = new Character("Khans");
 
+		std::cout << *khans;
+
+		Enemy*		ca = new CyberAnt();
+		Enemy*		sm = new SuperMutant();
+
+		AWeapon*	aa = new AlienAtomizer();
+		AWeapon*	pr = new PlasmaRifle();
+
+		khans->equip(aa);
+		std::cout << *khans;
+		khans->attack(ca);
+		std::cout << *khans;
+		khans->attack(ca);
+		std::cout << *khans;
+		khans->attack(ca);
+		std::cout << *khans;
+		khans->attack(ca);
+		std::cout << *khans;
+		khans->attack(ca);
+
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+
+		std::cout << *khans;
+		khans->attack(ca);
+		std::cout << *khans;
+		khans->attack(ca);
+
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+
+		khans->equip(pr);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+		khans->recoverAP();
+		std::cout << *khans;
+
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+		std::cout << *khans;
+		khans->attack(sm);
+
+		delete aa;
+		delete pr;
+		delete ca;
+		delete sm;
+		delete khans;
+	}
 	return 0;
 }
